277f41379abe9d4c07903960bf1284cedbd95f04
jimmyliu <jimmy85030@gmail.com>
2025-05-06 19:20:16 +0800

    feat: create postgres database connection

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"calorie_deficit/config"
+	// Import utils package early to load environment variables
+	"calorie_deficit/config" // Import config package early to set up environment variables
+	"calorie_deficit/infrastructure/database/postgres"
 	"calorie_deficit/routes"
 
+	// Import utils package early to load environment variables
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Initialize connection to PostgreSQL database
+	if err := postgres.ConnectPostgresDB(); err != nil {
+		panic(err)
+	}
+
 	// Create a new Gin router
 	router := gin.Default()
-
-	// Define a simple GET endpoint
+	// Register the routes with the router
 	routes.RegisterRoutes(router)
 
 	// Start the server on port 8080
--- utils/env.go
+++ utils/env.go
@@ -1,8 +1,18 @@
 package utils
 
 import (
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
+
+// init function is used to load environment variables from a .env file before the main function is called.
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Error loading .env file: %v", err)
+	}
+}
 
 // GetEnv retrieves the value of the environment variable named by the key.
 // If the variable is not found, it returns the provided default value.
@@ -11,5 +21,6 @@ func GetEnv(key, defaultValue string) string {
 	if !exists {
 		return defaultValue
 	}
+
 	return value
 }
